fd5c059533c385a494597c66e5825778e7e9e14c
kd1388588 <kd1388588@st.kobedenshi.ac.jp>
2026-05-29 11:55:26 +0900

    20260529 Player動作基礎完成 実行可能 画像迷い中

--- 先輩仮ゲー/Src/Application/Object/Player/Player.cpp
+++ 先輩仮ゲー/Src/Application/Object/Player/Player.cpp
@@ -2,25 +2,183 @@
 
 void Player::Init()
 {
-	m_polygon = std::make_shared<KdSquarePolygon>();
+	m_IdlePoly = std::make_shared<KdSquarePolygon>();
+	m_IdlePoly->SetMaterial("Asset/Textures/Object/Player/Idle.png");
 
-	m_polygon->SetMaterial("Asset/Textures/Object/Player/Idle.png");
+	m_Atk1Poly = std::make_shared<KdSquarePolygon>();
+	m_Atk1Poly->SetMaterial("Asset/Textures/Object/Player/Atk1.png");
 
-	m_pos = { -20,2,0 };
+	m_Atk2Poly = std::make_shared<KdSquarePolygon>();
+	m_Atk2Poly->SetMaterial("Asset/Textures/Object/Player/Atk2.png");
+	
+	m_RunPoly = std::make_shared<KdSquarePolygon>();
+	m_RunPoly->SetMaterial("Asset/Textures/Object/Player/Run.png");
+	
+	m_DashPoly = std::make_shared<KdSquarePolygon>();
+	m_DashPoly->SetMaterial("Asset/Textures/Object/Player/Dash.png");
 
-	m_polygon->SetSplit(7, 1);
+	m_pos = { 0,-1,0 };
 
-	m_polygon->SetPivot(KdSquarePolygon::PivotType::Center_Bottom);
+	m_IdlePoly->SetSplit(7, 1);
+	m_IdlePoly->SetPivot(KdSquarePolygon::PivotType::Center_Bottom);
+
+	m_Atk1Poly->SetSplit(10, 1);
+	m_Atk1Poly->SetPivot(KdSquarePolygon::PivotType::Center_Bottom);
+	
+	m_Atk2Poly->SetSplit(15, 1);
+	m_Atk2Poly->SetPivot(KdSquarePolygon::PivotType::Center_Bottom);
+	
+	//m_IdlePoly->SetSplit(7, 1);
+	//m_IdlePoly->SetPivot(KdSquarePolygon::PivotType::Center_Bottom);
+
+
+	m_pNowPolygon = m_IdlePoly;
 }
 
 void Player::Update()
 {
-	if (GetAsyncKeyState(VK_RIGHT) & 0x8000) { m_pos.x += 0.1f; }
-	if (GetAsyncKeyState(VK_LEFT) & 0x8000) { m_pos.x -= 0.1f; }
 
-	Math::Matrix transMat = Math::Matrix::CreateTranslation(m_pos);
+	// ダッシュ入力
+	if(GetAsyncKeyState(VK_SHIFT) & 0x8000)
+	{
+		m_dush = true;
+	}
+	else
+	{
+		m_dush = false;
+	}
 
-	m_mWorld = transMat;
+	// ダッシュ状態
+	if (m_dush == true)
+	{
+		if (GetAsyncKeyState('W') & 0x8000)
+		{
+			m_pos.z += 0.01f;
+		}
+
+		if (GetAsyncKeyState('S') & 0x8000)
+		{
+			m_pos.z -= 0.01f;
+		}
+
+		if (GetAsyncKeyState('A') & 0x8000)
+		{
+			m_pos.x -= 0.01f;
+		}
+		if (GetAsyncKeyState('D') & 0x8000)
+		{
+			m_pos.x += 0.01f;
+		}
+	}
+
+	// ダッシュしていない場合
+	else
+	{
+		if (GetAsyncKeyState('W') & 0x8000)
+		{
+			m_pos.z += 0.01f;
+		}
+
+		if (GetAsyncKeyState('S') & 0x8000)
+		{
+			m_pos.z -= 0.01f;
+		}
+
+		if (GetAsyncKeyState('A') & 0x8000)
+		{
+			m_pos.x -= 0.01f;
+		}
+		if (GetAsyncKeyState('D') & 0x8000)
+		{
+			m_pos.x += 0.01f;
+		}
+	}
+
+	// 攻撃入力
+	if (m_state == State::Idle)
+	{
+		// ダッシュしている場合
+		if (m_dush == true)
+		{
+			if (GetAsyncKeyState(VK_LBUTTON) & 0x8000)
+			{
+				m_state = State::Atk1;           // 状態を攻撃に変更
+				m_pNowPolygon = m_Atk1Poly;      // 使う画像を攻撃用に切り替え
+				m_anime = 0.0f;                    // アニメーションを0コマ目から再生
+			}
+		}
+
+		// ダッシュしていない場合
+		else 
+		{
+			if (GetAsyncKeyState(VK_LBUTTON) & 0x8000)
+			{
+			m_state = State::Atk2;           // 状態を攻撃に変更
+			m_pNowPolygon = m_Atk2Poly;      // 使う画像を攻撃用に切り替え
+			m_anime = 0.0f;                    // アニメーションを0コマ目から再生
+			}
+		}
+	}
+
+
+	// アニメーション処理
+	{
+		if (m_state == State::Idle)
+		{
+			animeX[7];
+			m_anime += 0.2f; // アニメーションの再生速度
+
+			// Idle状態：7コマでループ
+			if (m_anime >= 7.0f)
+			{
+				m_anime = 0.0f;
+			}
+		}
+		else if (m_state == State::Atk2)
+		{
+			animeX[15];
+			m_anime += 0.2f; // アニメーションの再生速度
+
+			// Attack状態：10コマ再生し終わったらIdleに戻る
+			if (m_anime >= 15.0f)
+			{
+				m_state = State::Idle;         // 状態を待機に戻す
+				m_pNowPolygon = m_IdlePoly;    // 画像を待機用に切り替え
+				m_anime = 0.0f;                // アニメーションをリセット
+			}
+		}
+		else if (m_state == State::Atk1)
+		{
+			animeX[10];
+			m_anime += 0.2f; // アニメーションの再生速度
+
+			// Attack状態：10コマ再生し終わったらIdleに戻る
+			if (m_anime >= 10.0f)
+			{
+				m_state = State::Idle;         // 状態を待機に戻す
+				m_pNowPolygon = m_IdlePoly;    // 画像を待機用に切り替え
+				m_anime = 0.0f;                // アニメーションをリセット
+			}
+		}
+
+		// アニメーション処理
+		m_pNowPolygon->SetUVRect(animeX[(int)m_anime]);
+
+	// ------------------------------------------------------------
+	}
+
+	// 行列の更新
+	{
+		// 拡縮行列
+		Math::Matrix m_scale = Math::Matrix::CreateScale(0.5f, 1.0f, 1.0f);
+
+		// 移動行列
+		Math::Matrix transMat = Math::Matrix::CreateTranslation(m_pos);
+
+		// 行列合成
+		m_mWorld = m_scale * transMat;
+	}
+
 }
 
 void Player::PostUpdate()
@@ -28,8 +186,12 @@ void Player::PostUpdate()
 
 }
 
-void Player::DrawSprite()
+void Player::DrawLit()
 {
+	if (!m_pNowPolygon) return;
+
+	Math::Color color = { 0,1,0,1 };
+
 	KdShaderManager::Instance().
-		m_StandardShader.DrawPolygon(*m_polygon);
+		m_StandardShader.DrawPolygon(*m_pNowPolygon,m_mWorld);
 }
--- 先輩仮ゲー/Src/Application/Object/Player/Player.h
+++ 先輩仮ゲー/Src/Application/Object/Player/Player.h
@@ -4,6 +4,19 @@ class Player : public KdGameObject
 {
 
 public:
+	// プレイヤーの状態を管理する列挙型
+	enum class State
+	{
+		Idle,		// 操作無
+		Hurt,		// 被弾
+		Death,		// 死亡
+		Run,		// 移動
+		Dash,		// ダッシュ
+		Jump,		// ジャンプ
+		Fall,		// 落下
+		Atk1,		// 通常攻撃
+		Atk2,		// ダッシュ攻撃
+	};
 
 	Player() { Init(); }
 	~Player() override {}
@@ -13,7 +26,7 @@ public:
 	void Update()		override;
 	void PostUpdate()	override;
 	
-	void DrawSprite()	override;
+	void DrawLit()	override;
 
 private:
 
@@ -21,11 +34,30 @@ private:
 	Math::Vector3 m_pos;
 
 	// 画像
-	std::shared_ptr<KdSquarePolygon> m_polygon;
+	std::shared_ptr<KdSquarePolygon> m_IdlePoly;	// 操作無
+	std::shared_ptr<KdSquarePolygon> m_HurtPoly;	// 被弾
+	std::shared_ptr<KdSquarePolygon> m_DeathPoly;	// 死亡
+	std::shared_ptr<KdSquarePolygon> m_RunPoly;		// 移動
+	std::shared_ptr<KdSquarePolygon> m_DashPoly;	// ダッシュ
+	std::shared_ptr<KdSquarePolygon> m_JumpPoly;	// ジャンプ
+	std::shared_ptr<KdSquarePolygon> m_FallPoly;	// 落下
+	std::shared_ptr<KdSquarePolygon> m_Atk1Poly;	// 通常攻撃
+	std::shared_ptr<KdSquarePolygon> m_Atk2Poly;	// ダッシュ攻撃
 	
+	std::shared_ptr<KdSquarePolygon> m_pNowPolygon; // 現在の画像
+
+	// 状態
+	State m_state = State::Idle;
+
 	// アニメーション
+	int animeX[15] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14 };
 	float m_anime = 0.0f;
 
 	// 重力
 	float m_gravity = 0;
+
+	// ダッシュ入力
+	bool m_dush = false;
+
+
 };
